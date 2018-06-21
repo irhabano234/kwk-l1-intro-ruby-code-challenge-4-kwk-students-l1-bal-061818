da2835904d3a7bc1445375a74fb6b40d3a94e88e
irhabano234 <irha.bano@gmail.com>
2018-06-21 15:26:49 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -1,7 +1,7 @@
 #write out your code here
-coins = ["quarters", "dimes", "nickels", "pennies"]
+coins = ["quarters" => 25 , "dimes" => 10, "nickels" => 5, "pennies" => 1]
 
-def least_coins(cents)
+def least_coins(29)
 
 #Code your answer here!
 
